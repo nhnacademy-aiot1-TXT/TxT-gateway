195922318808e74a6d9aee067509494e12b587b3
pass0210 <pass0210@gmail.com>
2024-03-26 13:51:24 +0900

    Feat: authorization header check filter 구현 완료
    Authorization 헤더를 확인하는 필터를 구현하고 Order를 구현하여 필터들 중에서 먼저 실행하도록 설정

---
+++ src/main/java/com/nhnacademy/gateway/filter/AuthorizationHeaderCheckFilter.java
@@ -0,0 +1,55 @@
+package com.nhnacademy.gateway.filter;
+
+import com.nhnacademy.gateway.properties.ExcludePathProperties;
+import com.nhnacademy.gateway.utils.ExceptionUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Component
+public class AuthorizationHeaderCheckFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderCheckFilter.Config> implements Ordered {
+    private final ExceptionUtil exceptionUtil;
+    private final List<String> excludePathList;
+
+    public AuthorizationHeaderCheckFilter(ExceptionUtil exceptionUtil, ExcludePathProperties excludePathProperties) {
+        this.exceptionUtil = exceptionUtil;
+        this.excludePathList = List.of(excludePathProperties.getPath().split(","));
+    }
+
+    public static class Config {
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            log.debug("authorization header check filter");
+            ServerHttpRequest request = exchange.getRequest();
+            URI uri = request.getURI();
+
+            if (excludePathList.contains(uri.getPath())) {
+                return chain.filter(exchange);
+            }
+
+            if (Objects.isNull(request.getHeaders().get(HttpHeaders.AUTHORIZATION)) ||
+                !request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return exceptionUtil.exceptionHandler(exchange, "not exist authorization header");
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
