23e28566964db738bd357c2fa5517bd8abcc5115
pass0210 <pass0210@gmail.com>
2024-03-26 10:23:52 +0900

    Feat: ExceptionUtil 구현 완료

---
+++ src/main/java/com/nhnacademy/gateway/utils/ExceptionUtil.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.gateway.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ExceptionUtil {
+    private Mono<Void> exceptionHandler(ServerWebExchange exchange, String message) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+        return response.setComplete();
+    }
+}
