77fa74ee39b5304cbe890b319bb1929955deb0b8
pass0210 <pass0210@gmail.com>
2024-03-26 17:00:11 +0900

    Feat: AddUserIdHeaderFilter 구현 완료

---
+++ src/main/java/com/nhnacademy/gateway/filter/AddUserIdHeaderFilter.java
@@ -0,0 +1,58 @@
+package com.nhnacademy.gateway.filter;
+
+import com.nhnacademy.gateway.properties.ExcludePathProperties;
+import com.nhnacademy.gateway.properties.JwtProperties;
+import com.nhnacademy.gateway.utils.JwtProvider;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.util.List;
+
+@Component
+public class AddUserIdHeaderFilter extends AbstractGatewayFilterFactory<AddUserIdHeaderFilter.Config> implements Ordered {
+    private final JwtProperties jwtProperties;
+    private final JwtProvider jwtProvider;
+    private final List<String> excludePathList;
+
+    public AddUserIdHeaderFilter(JwtProperties jwtProperties, JwtProvider jwtProvider, ExcludePathProperties excludePathProperties) {
+        super(Config.class);
+        this.jwtProperties = jwtProperties;
+        this.jwtProvider = jwtProvider;
+        this.excludePathList = List.of(excludePathProperties.getPath().split(","));
+    }
+
+    public static class Config {
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            URI uri = request.getURI();
+
+            if (excludePathList.contains(uri.getPath())) {
+                return chain.filter(exchange);
+            }
+
+            String accessToken = jwtProvider.extractAuthorizationHeader(request)
+                    .replace(jwtProperties.getTokenPrefix(), "")
+                    .trim();
+
+            String userId = jwtProvider.getUserId(accessToken);
+
+            exchange.mutate()
+                    .request(builder -> builder.header("X-USER-ID", userId));
+
+            return chain.filter(exchange);
+        };
+    }
+
+    @Override
+    public int getOrder() {
+        return 2;
+    }
+}
