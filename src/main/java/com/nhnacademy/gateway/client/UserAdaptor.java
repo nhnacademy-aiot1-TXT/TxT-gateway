7117a88adb9ad0dd15bdc69f8f2f4c1e8a25f393
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-03-27 13:22:03 +0900

    Docs: client package 하위 클래스 javadoc 생성

--- src/main/java/com/nhnacademy/gateway/client/UserAdaptor.java
+++ src/main/java/com/nhnacademy/gateway/client/UserAdaptor.java
@@ -11,12 +11,24 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 
+/**
+ * 인증 서버로 토큰 재발급 요청을 보내는 클래스
+ * @author gahyoung
+ * @version 1.0
+ */
 @Component
 @RequiredArgsConstructor
 public class UserAdaptor {
     private static final String X_USER_ID = "X-USER-ID";
     private final RestTemplate restTemplate;
 
+    /**
+     * 인증 서버로 토큰 재발급 요청을 보내는 메서드
+     *
+     * @param userId 토큰 재발급에 필요한 user ID
+     * @param refreshToken 토큰 재발급에 필요한 리프레시 토큰
+     * @return 재발급 받은 토큰 반환
+     */
     public AccessTokenResponse reissueToken(String userId, String refreshToken){
 
         HttpHeaders headers = new HttpHeaders();
