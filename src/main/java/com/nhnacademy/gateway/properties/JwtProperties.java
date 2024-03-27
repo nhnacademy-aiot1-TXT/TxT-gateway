0b6a958101417aeff9f71ec8b57a5a9832afa333
pass0210 <pass0210@gmail.com>
2024-03-27 15:21:51 +0900

    Refactor: JwtProperties 오타 수정

--- src/main/java/com/nhnacademy/gateway/properties/JwtProperties.java
+++ src/main/java/com/nhnacademy/gateway/properties/JwtProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * jwt관련 기능을 수행할 때 필요한 정보를 저하기 위한 클래스
+ * jwt관련 기능을 수행할 때 필요한 정보를 저장하기 위한 클래스
  *
  * @author parksangwon
  * @version 1.0
