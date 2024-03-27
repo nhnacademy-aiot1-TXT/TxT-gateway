af435e7468281c7ac2d5deaf11e497f896b50bdd
pass0210 <pass0210@gmail.com>
2024-03-27 15:14:30 +0900

    Refactor: filter 제외 이유 추가 및 오타 수정

--- src/main/java/com/nhnacademy/gateway/properties/ExcludePathProperties.java
+++ src/main/java/com/nhnacademy/gateway/properties/ExcludePathProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * login, signup 등 token을 사용하지 않는 요청은 filter에서 제외
  * filter를 적용하지 않는 path를 저장하기 위한 클래스
  *
  * @author parksangwon
