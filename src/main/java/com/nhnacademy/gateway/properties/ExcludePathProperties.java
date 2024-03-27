9e531fd61ae699979ab8e17b6af0074eccd2bb09
pass0210 <pass0210@gmail.com>
2024-03-27 13:34:37 +0900

    Docs: properties javadoc 문서 작업 완료
    javadoc을 추가하여 클래스의 기능을 설명

--- src/main/java/com/nhnacademy/gateway/properties/ExcludePathProperties.java
+++ src/main/java/com/nhnacademy/gateway/properties/ExcludePathProperties.java
@@ -5,6 +5,12 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * filter를 적용하지 않는 path를 저장하기 위한 클래스
+ *
+ * @author parksangwon
+ * @version 1.0
+ */
 @Getter
 @Setter
 @Configuration
--- src/main/java/com/nhnacademy/gateway/properties/JwtProperties.java
+++ src/main/java/com/nhnacademy/gateway/properties/JwtProperties.java
@@ -5,6 +5,12 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * jwt관련 기능을 수행할 때 필요한 정보를 저하기 위한 클래스
+ *
+ * @author parksangwon
+ * @version 1.0
+ */
 @Getter
 @Setter
 @Configuration
