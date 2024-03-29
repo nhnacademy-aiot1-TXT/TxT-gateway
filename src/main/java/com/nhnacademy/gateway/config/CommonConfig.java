93e508ad283ab246a4a5a4588497fa71cb6b9fda
pass0210 <pass0210@gmail.com>
2024-03-29 16:03:43 +0900

    Add: 복호화 메서드 추가 및 로직 추가

--- src/main/java/com/nhnacademy/gateway/config/CommonConfig.java
+++ src/main/java/com/nhnacademy/gateway/config/CommonConfig.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.gateway.config;
 
+import com.nhnacademy.gateway.exception.CryptoOperationException;
 import com.nhnacademy.gateway.properties.JwtProperties;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
@@ -8,8 +9,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.time.Duration;
+import java.util.Base64;
 
 /**
  * 토큰 검증을 위한 Parser 및 토큰 요청을 보내기 위한 config 클래스
@@ -23,14 +29,25 @@ public class CommonConfig {
     /**
      * 토큰 검증을 위한 JwtParser 빈 등록 메서드
      *
-     * @param jwtProperties 토큰 정보가 담겨있는 프로퍼티
      * @return 토큰을 parsing 하기 위한 JwtParser 인스턴스 반환
      */
     @Bean
-    public JwtParser jwtParser(JwtProperties jwtProperties) {
+    public JwtParser jwtParser() {
         return Jwts.parserBuilder()
-                .setSigningKey(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8))
+                .setSigningKey(getPublicKeyDecryption(null))
                 .build();
+    }
+
+    @Bean
+    public PublicKey getPublicKeyDecryption(JwtProperties jwtProperties) {
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] keyBytes = Base64.getDecoder().decode(jwtProperties.getSecret());
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
+            return keyFactory.generatePublic(x509EncodedKeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new CryptoOperationException("JwtService: " + e);
+        }
     }
 
     /**
