f8fcba03e0501d4140e7b587fcecfaf8c8211e48
pass0210 <pass0210@gmail.com>
2024-06-01 16:11:06 +0900

    Docs: CryptoOperationException JavaDoc 추가

--- src/main/java/com/nhnacademy/gateway/exception/CryptoOperationException.java
+++ src/main/java/com/nhnacademy/gateway/exception/CryptoOperationException.java
@@ -1,6 +1,14 @@
 package com.nhnacademy.gateway.exception;
 
-public class CryptoOperationException extends RuntimeException{
+/**
+ * 암호화 key 생성 중 발생하는 예외를 처리하기 위한 클래스
+ */
+public class CryptoOperationException extends RuntimeException {
+    /**
+     * message를 파라미터로 가지는 생성자 메서드
+     *
+     * @param message 얘외 메시지
+     */
     public CryptoOperationException(String message) {
         super(message);
     }
